7bf812d3306517c03bedafc3326306f503c6bec4
Muhammad Usman <usmantufail837@gmail.com>
2024-11-04 11:09:12 +0500

    create banner component and add background image

---
+++ src/app/Components/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Banner = () => {
+  return (
+    <div className="h-screen bg-cover bg-center" style={{ backgroundImage: "url('/Assets/banner-bg.jpg')"}}>
+      
+    </div>
+  )
+}
+
+export default Banner
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import Header from "./Components/Header"
-import Navbar from "./Components/Navbar";
+import Navbar from "./Components/Navbar"
+import Banner from "./Components/Banner"
 
 export default function Home() {
   return (
     <div>
       <Header/>
       <Navbar/>
+      <Banner/>
     </div>
   );
 }
