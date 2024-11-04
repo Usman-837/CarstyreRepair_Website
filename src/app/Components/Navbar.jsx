1e8ac10e377bbf5827ce2cb3798beb774ec43545
Muhammad Usman <usmantufail837@gmail.com>
2024-11-04 13:35:59 +0500

    reset position in in navbar

--- src/app/Components/Navbar.jsx
+++ src/app/Components/Navbar.jsx
@@ -4,8 +4,8 @@ import Image from 'next/image'
 
 function Navbar() {
   return (
-    <nav className='flex justify-center z-50'>
-        <div className='flex justify-between items-center w-full max-w-[1200px] bg-[#FFFFFF] h-[76px] py-2 px-2 absolute top-24 max-lg:top-0'>
+    <nav className='flex justify-center z-50 relative'>
+        <div className='flex justify-between items-center w-full max-w-[1200px] bg-[#FFFFFF] h-[76px] py-2 px-2 absolute top-[-35px] max-lg:top-0'>
             {/* logo */}
             <Image src={logo} className='w-[150px] h-[60px]'/>
             <div>
