6c91a330eef5d15321a3f390d20255fe46036ce5
Muhammad Usman <usmantufail837@gmail.com>
2024-11-04 12:34:35 +0500

    add banenr image in banner section

--- src/app/Components/Banner.jsx
+++ src/app/Components/Banner.jsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React from 'react'
+import BannerImg from "../Assets/banner-image.webp"
+import Image from 'next/image'
+
 
 const Banner = () => {
   return (
-    <div className="h-screen bg-cover bg-center" style={{ backgroundImage: "url('/Assets/banner-bg.jpg')" }}>
+    <section className="relative h-screen bg-cover bg-center flex justify-center" style={{ backgroundImage: "url('/Assets/banner-bg.jpg')" }}>
       {/* Overlay */}
-      <div className="h-full w-full text-white bg-black bg-opacity-75 flex items-center justify-center">
+      <div className="absolute inset-0 bg-black bg-opacity-75" />
+      {/* Content */}
+      <div className="relative w-full max-w-[1200px] h-full text-white flex items-center justify-center z-10">
         <div>
           <p className="mb-4 text-base font-bold">\ Welcome to car tyre repair</p>
           <h1 className='text-6xl font-bold mb-2'>Fastest on-site Tyre Repair</h1>
@@ -13,8 +18,11 @@ const Banner = () => {
               <a href="#">Contact Now</a>
           </div>
         </div>
+        <div>
+            <Image src={BannerImg}/>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
