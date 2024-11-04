f78b5fa9b44fff802167bfc22c08d6d45920c2a6
Muhammad Usman <usmantufail837@gmail.com>
2024-11-04 16:08:25 +0500

    create services component and add headings

---
+++ src/app/Components/Services.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Services = () => {
+  return (
+    <section className='flex items-center justify-center'>
+        <div className='w-full max-w-[1200px]'>
+            <p className="mb-4 px-3 text-base font-bold text-[#BB0612]">// Our Services //</p>
+            <h2 className='mb-2 px-3 font-semibold text-4xl'>Our Best Features</h2>
+        </div>
+    </section>
+  )
+}
+
+export default Services
