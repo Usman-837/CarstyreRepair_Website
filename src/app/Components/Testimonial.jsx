c31109553876f612edefaebd83e08dfaa895b184
Muhammad Usman <usmantufail837@gmail.com>
2024-11-05 14:48:22 +0500

    add heading and buttons to slide sliders

--- src/app/Components/Testimonial.jsx
+++ src/app/Components/Testimonial.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import Image from 'next/image'
 import Testimonialimg1 from "../Assets/Testimonials.webp"
+import { MdOutlineKeyboardArrowLeft } from "react-icons/md"
+import { MdKeyboardArrowRight } from "react-icons/md"
 
 export default function Testimonial() {
   return (
@@ -8,6 +10,17 @@ export default function Testimonial() {
         <div className='flex items-center justify-center'>
             <div>
                 <Image src={Testimonialimg1}/>
+            </div>
+            <div className='w-full max-w-[700px] py-12 px-3'>
+                <p className='text-[#BB0612] font-bold mb-4'>// What Say Our Clients //</p>
+                <div className='flex justify-between mb-4'>
+                  <h2 className='font-semibold text-4xl'>Our Testimonials</h2>
+                  <div className='flex text-3xl gap-4'>
+                    <button className='border border-black p-1.5'><MdOutlineKeyboardArrowLeft /></button>
+                    <button className='border border-black p-1.5'><MdKeyboardArrowRight /></button>
+                  </div>
+                </div>
+
             </div>
         </div>
     </section>
