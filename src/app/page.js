00056ed84d97579cffc311e360c2ad8bbd31cdd5
Muhammad Usman <usmantufail837@gmail.com>
2024-11-06 15:53:20 +0500

    create whatsapp component and add whatsapp icon in footer

--- src/app/page.js
+++ src/app/page.js
@@ -6,6 +6,7 @@ import Services from "./Components/Services";
 import ChooseUs from "./Components/ChooseUs";
 import Testimonial from "./Components/Testimonial";
 import Footer from "./Components/Footer";
+import Whatsapp from "./Components/Whatsapp";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
       <ChooseUs/>
       <Testimonial/>
       <Footer/>
+      <Whatsapp/>
     </div>
   );
 }
