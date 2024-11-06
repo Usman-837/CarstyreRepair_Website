f5f1d817c2877ec09832596a79029f8b74df08c4
Muhammad Usman <usmantufail837@gmail.com>
2024-11-06 16:04:03 +0500

    update header

--- src/app/Components/Header.jsx
+++ src/app/Components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { CiLocationOn } from "react-icons/ci"
 import { TfiEmail } from "react-icons/tfi"
 import { FaFacebookF } from "react-icons/fa6"
-import { TiSocialLinkedin } from "react-icons/ti"
+import { FaLinkedinIn } from "react-icons/fa"
 import { FaInstagram } from "react-icons/fa6"
 import { FaTwitter } from "react-icons/fa"            
 
@@ -10,35 +10,35 @@ import { FaTwitter } from "react-icons/fa"
 const Header = () => {
   return (
     <header className='bg-[#222222] text-white flex justify-center pt-4 max-lg:hidden'>
-        <div className='w-full max-w-[1200px] flex justify-between  pb-[55px]'>
+        <div className='container w-full flex justify-between pb-fiftyfive mx-twintytwo px-3'>
             {/* Location */}
-            <div className='flex gap-3 items-center'>
-                <CiLocationOn  className='text-3xl text-[#A00B15]'/>
+            <div className='flex gap-3 items-center justify-center'>
+            <CiLocationOn className="text-4xl text-red stroke-[.5px]" />
                 <div>
                     <h5 className='font-semibold text-lg'>Office Location</h5>
-                    <p className='text-[15px]'>Dubai</p>
+                    <p className='text-fifteen'>Dubai</p>
                 </div>
             </div>
             {/* Mail */}
-            <div className='flex gap-3 items-center'>
-                <TfiEmail className='text-3xl text-[#A00B15]'/>
+            <div className='flex gap-3 items-center justify-center'>
+                <TfiEmail className='text-3xl text-red'/>
                 <div>
                     <h5 className='font-semibold text-lg'>Mail Us</h5>
-                    <p className='text-[15px]'><a href=''>info@carstyrerepair.com</a></p>
+                    <p className='text-fifteen'><a href=''>info@carstyrerepair.com</a></p>
                 </div>
             </div>
             {/* Social Icons */}
-            <div className='flex items-center'>
-                <ul className='flex gap-3'>
+            <div className='flex items-center justify-center'>
+                <ul className='flex gap-3 text-seventeen'>
                     <li><a href="#"><FaFacebookF /></a></li>
-                    <li><a href="#"><TiSocialLinkedin /></a></li>
+                    <li><a href="#"><FaLinkedinIn /></a></li>
                     <li><a href="#"><FaInstagram /></a></li>
                     <li><a href="#"><FaTwitter /></a></li>
                 </ul>
             </div>
             {/* Appointment Button */}
-            <div className='flex items-center'>
-                <a href='#' className='bg-[#BB0612] text-center text-sm py-[15px] px-[35px] hover:bg-transparent'>                
+            <div className='flex items-center justify-center'>
+                <a href='#' className='bg-red text-sm py-fifteen px-thirtyfive hover:bg-transparent'>                
                     Take Appointment
                 </a>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,51 @@ module.exports = {
         foreground: "var(--foreground)",
       },
       fontFamily: {
-        montserrat: ['Montserrat', 'sans-serif'],
+        sans: ['Montserrat', 'sans-serif'],
+      },
+      roboto: ['Roboto', 'sans-serif'], // Custom font class for Roboto
+      opacity: {
+        85: '0.85', // For example, 85% opacity
+      },
+      lineHeight: {
+        'custom-1.9': '1.9',
+      },
+      fontSize: {
+        fifteen: '15px',
+        twintytwo: '22px',
+      },
+      padding: {
+        fifteen: '15px',
+        thirtyfive: '35px',
+        fiftyfive: '55px'
+      },
+      margin: {
+        twintytwo: '22px',
+        '5': '5px'
+      },
+      width: {
+        'onefifty': '150px',
+        'twenty': '20px',
+        'seven': '7px',
+        'five': '5px'
+      },
+      maxWidth: {
+        '1359': '1350px',
+        '600': '600px',
+        '576': '576px',
+        '300': '300px',
+        '200': '200px'
+      },
+      height: {
+        'sixty': '60px',
+        'twenty': '20px',
+        'seven': '7px'
+      },
+      screens: {
+        'large-tab': '993px',
+      },
+      colors: {
+        'red': '#BB0612',
       },
     },
   },
