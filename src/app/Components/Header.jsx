b3a32154f2f95fff21f86f60948b5779ab95601f
Muhammad Usman <usmantufail837@gmail.com>
2024-11-06 16:35:28 +0500

    update header

--- src/app/Components/Header.jsx
+++ src/app/Components/Header.jsx
@@ -2,11 +2,7 @@ import React from 'react'
 import { CiLocationOn } from "react-icons/ci"
 import { TfiEmail } from "react-icons/tfi"
 import { FaFacebookF } from "react-icons/fa6"
-<<<<<<< HEAD
 import { FaLinkedinIn } from "react-icons/fa"
-=======
-import { FaLinkedinIn } from "react-icons/fa";
->>>>>>> main
 import { FaInstagram } from "react-icons/fa6"
 import { FaTwitter } from "react-icons/fa"            
 
@@ -17,11 +13,7 @@ const Header = () => {
         <div className='container w-full flex justify-between pb-fiftyfive mx-twintytwo px-3'>
             {/* Location */}
             <div className='flex gap-3 items-center justify-center'>
-<<<<<<< HEAD
             <CiLocationOn className="text-4xl text-red stroke-[.5px]" />
-=======
-            <CiLocationOn className="text-4xl text-[#A00B15] stroke-[.5px]" />
->>>>>>> main
                 <div>
                     <h5 className='font-semibold text-lg'>Office Location</h5>
                     <p className='text-fifteen'>Dubai</p>
@@ -29,11 +21,7 @@ const Header = () => {
             </div>
             {/* Mail */}
             <div className='flex gap-3 items-center justify-center'>
-<<<<<<< HEAD
                 <TfiEmail className='text-3xl text-red'/>
-=======
-                <TfiEmail className='text-3xl text-[#A00B15]'/>
->>>>>>> main
                 <div>
                     <h5 className='font-semibold text-lg'>Mail Us</h5>
                     <p className='text-fifteen'><a href=''>info@carstyrerepair.com</a></p>
@@ -50,11 +38,7 @@ const Header = () => {
             </div>
             {/* Appointment Button */}
             <div className='flex items-center justify-center'>
-<<<<<<< HEAD
                 <a href='#' className='bg-red text-sm py-fifteen px-thirtyfive hover:bg-transparent'>                
-=======
-                <a href='#' className='bg-[#BB0612] text-sm py-fifteen px-thirtyfive hover:bg-transparent'>                
->>>>>>> main
                     Take Appointment
                 </a>
             </div>
