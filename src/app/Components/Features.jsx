c09ef1716ef078c93091a5cb75cf556e4dde3da9
Muhammad Usman <usmantufail837@gmail.com>
2024-11-07 12:39:56 +0500

    add responsive functionality in features component to make it responsive

--- src/app/Components/Features.jsx
+++ src/app/Components/Features.jsx
@@ -4,24 +4,24 @@ import Image from 'next/image'
 
 const Features = () => {
     return (
-        <section className='py-[90px] flex items-center justify-center'>
-            <div className='w-full max-w-[1200px] flex justify-between'>
-                <div className='flex-1 flex justify-center'>
-                    <Image src={img} className='px-3 w-full max-w-[512px] h-[550px]' alt="Feature image" />
+        <section id='about' className='py-[90px] flex items-center justify-center'>
+            <div className='container w-full flex gap-x-16 justify-between max-md:flex-col-reverse max-md:gap-y-8 max-large-tab:gap-x-0 max-md:px-3'>
+                <div className='flex-1 flex justify-center max-w-[512px] h-[550px] px-3 max-md:px-3'>
+                    <Image src={img} alt="Feature image" />
                 </div>
-                <div className='flex-1 flex flex-col pl-3 pr-12'>
-                    <p className="mb-2 text-base font-bold text-[#BB0612]">// Look At Our Quality Features \\</p>
-                    <h2 className='mb-2 font-semibold text-4xl'>Car Tyre Repaire is the Best Place For Your Tyre Care</h2>
-                    <h6 className='mb-2 py-3 pr-12 pe-md-5'>Get your tyres repaired by experienced professionals for guaranteed safety. Check our reviews and join over 300+ happy customers!</h6>
-                    <ul class="list-disc pl-5 space-y-2">
-                        <li>Onsite tyre service in case of tyre puncture.</li>
-                        <li>Tyre repair at home or at your location.</li>
-                        <li>Tyre puncture and tyre change service.</li>
-                        <li>Tyre Replacement or tyre change.</li>
-                        <li>Tyre Replacement or tyre change.</li>
+                <div className='flex-1 flex flex-col pl-3 pr-12 max-md:pr-0 max-md:pl-0'>
+                    <p className="mb-2 text-base font-bold text-red">// Look At Our Quality Features //</p>
+                    <h2 className='mb-2 font-semibold text-4xl max-md:text-2xl'>Car Tyre Repaire is the Best Place For Your Tyre Care</h2>
+                    <h6 className='mb-2 py-4 pr-12 max-md:pr-0'>Get your tyres repaired by experienced professionals for guaranteed safety. Check our reviews and join over 300+ happy customers!</h6>
+                    <ul class="list-disc pl-4 text-lg">
+                        <li className='mb-2'>Onsite tyre service in case of tyre puncture.</li>
+                        <li className='mb-2'>Tyre repair at home or at your location.</li>
+                        <li className='mb-2'>Tyre puncture and tyre change service.</li>
+                        <li className='mb-2'>Tyre Replacement or tyre change.</li>
+                        <li className='mb-2'>Tyre Replacement or tyre change.</li>
                     </ul>
-                    <div className='flex items-center text-white mt-5'>
-                        <a href='#' className='bg-[#BB0612] text-center text-sm py-[15px] px-[35px] hover:bg-[#222222]'>
+                    <div className='flex items-center text-white mt-12'>
+                        <a href='#' className='bg-red text-center text-sm py-fifteen px-thirtyfive hover:bg-custom-black'>
                             Call Now +971557474109
                         </a>
                     </div>
