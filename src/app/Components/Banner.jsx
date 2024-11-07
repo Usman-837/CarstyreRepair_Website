93fa1810215f01f348de162c0dd50f6837722484
Muhammad Usman <usmantufail837@gmail.com>
2024-11-07 11:35:39 +0500

    add responsive functionality in banner

--- src/app/Components/Banner.jsx
+++ src/app/Components/Banner.jsx
@@ -7,19 +7,19 @@ const Banner = () => {
   return (
     <section className="relative h-screen bg-cover bg-center flex justify-center" style={{ backgroundImage: "url('/Assets/banner-bg.jpg')" }}>
       {/* Overlay */}
-      <div className="absolute inset-0 bg-black bg-opacity-75" />
+      <div className="absolute inset-0 bg-black bg-opacity-75"/>
       {/* Content */}
-      <div className="relative w-full max-w-[1200px] h-full text-white flex items-center justify-center z-10">
-        <div>
+      <div className="relative container w-full h-full text-white flex items-center justify-center z-10 max-md:flex-wrap">
+        <div className='py-20 pl-12 max-w-600 max-md:pb-0 max-md:pt-28 max-md:flex max-md:flex-col max-md:justify-center max-md:mt-5'>
           <p className="mb-4 text-base font-bold">\ Welcome to car tyre repair</p>
-          <h1 className='text-6xl font-bold mb-2'>Fastest on-site Tyre Repair</h1>
-          <h2 className='my-6 text-4xl font-semibold'>& Tyre Services in Whole DUBAI in 20min</h2>
-          <div className="bg-[#BB0612] w-[164.3px] text-white py-[15px] px-[35px] hover:bg-[#222222] text-sm border-t border-white">
+          <h1 className='text-6xl font-bold mb-2 max-md:text-4xl'>Fastest on-site Tyre Repair</h1>
+          <h2 className='my-6 text-4xl font-semibold max-md:text-2xl'>& Tyre Services in Whole DUBAI in 20min</h2>
+          <div className="bg-red w-[164.3px] text-white py-fifteen px-thirtyfive hover:bg-custom-black text-sm border-t border-white">
               <a href="#">Contact Now</a>
           </div>
         </div>
-        <div>
-            <Image src={BannerImg}/>
+        <div className='pb-14'> 
+            <Image src={BannerImg} className='max-md:w-fit'/>
         </div>
       </div>
     </section>
